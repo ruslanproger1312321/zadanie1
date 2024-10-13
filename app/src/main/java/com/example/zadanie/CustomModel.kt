d37c09c790d035ccebcf2113ce2b8e30c1c08358
ruslanproger13112321 <ergazhyruslan00@gmail.com>
2024-10-13 23:32:39 +0500

    Imit commit

---
+++ app/src/main/java/com/example/zadanie/CustomModel.kt
@@ -0,0 +1,2 @@
+package com.example.zadanie
+data class CustomModel(val id:String,val name:String,val description:String,val date:Int)
---
+++ app/src/main/java/com/example/zadanie/Today.kt
@@ -0,0 +1,70 @@
+package com.example.zadanie
+
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+
+
+class Today : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.today)
+        val autorization:Toolbar=findViewById(R.id.toolbar)
+        setSupportActionBar(autorization)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.alltasks-> {
+                val alltasks=Intent(this, AllTasks::class.java)
+                startActivity(alltasks)
+            }
+            R.id.registr-> {
+                val autorization=Intent(this, Registr::class.java)
+                startActivity(autorization)
+            }
+            R.id.completed-> {
+                val completed=Intent(this, Completed::class.java)
+                startActivity(completed)
+            }
+            R.id.profile-> {
+                val profile=Intent(this, Profile::class.java)
+                startActivity(profile)
+            }
+            R.id.today-> {
+                val today=Intent(this, Today::class.java)
+                startActivity(today)
+            }
+            R.id.favorites-> {
+                val favorites=Intent(this, Favorites::class.java)
+                startActivity(favorites)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+}
